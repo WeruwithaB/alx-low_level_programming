214a3481d58919e335d99dabc834780b0022cd5d
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-08 15:42:44 -0700

    Initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
-
 #include <string.h>
 
-
-
 /**
+ * main - Main function of my project
  *
- * *main - Main function of my project
- *
- * *
- *
- * *
  *
  * *Return: return 0 everytime
  *
- * */
-
+ */
 int main(void)
-
 {
-
-
-
+	
 		char displayed[52];
 
-
-
 			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-
 				puts(displayed);
-
 					return (0);
 
 }
