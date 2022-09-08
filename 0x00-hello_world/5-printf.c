55efabe2dca79b601c9253d9c5f9c3cb425c8365
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-08 16:12:53 -0700

    Initial commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- /**
+/**
  *main - Main function
  *
  *
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
