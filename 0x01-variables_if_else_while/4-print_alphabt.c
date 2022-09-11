5dde866ba34094a058f5b1198cbc38d69a820fd4
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-11 13:10:39 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * variable assignment and type
+ *
+ * while function
+ *
+ * else if function
+ *
+ * putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph - 'a';
+
+	while (alph <= 'z')
+	{
+		if (alph == 'q')
+		{
+			alph++;
+		}
+		else if (alph == 'e')
+		{
+			alph++;
+		}
+		else 
+		{
+			putchar(alph);
+			alph++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
