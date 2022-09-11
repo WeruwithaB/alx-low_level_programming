39dc850291d439fa263c2cb50fb54a55849dc39a
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-11 13:57:00 -0700

    Initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ *
+ * Variable assignment and type
+ *
+ * forloop
+ * putchar function
+ *
+ * variable reassignment
+ *
+ * forloop
+ * putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hex = '0';
+
+	for (hex = '0'; hex <= '9'; hexx++)
+		putchar(hex);
+	{
+	int hex = 'a';
+
+		for (hex = 'a'; hex <= 'f'; hex++)
+			putchar(hex);
+				putchar('\n');
+	}
+	return (0);
+}
