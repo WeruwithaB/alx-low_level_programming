00e8d897182608efb16cb02759dda43d51b286d1
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 16:23:39 -0700

    Low Level Programming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,27 +8,27 @@
 
 void jack_bauer(void)
 {
-int a, b, c, d;
+	int a, b, c, d;
 
-for (a = 48; a <= 50; a++)
-{
-	for (b = 48; b <= 57; b++)
+	for (a = 48; a <= 50; a++)
 	{
-	if ((a <= 49 && b <= 57) || (a <= 50 && b <= 51))
-			{
-				for (c = 48; c <= 53; c++)
+		for (b = 48; b <= 57; b++)
+		{
+		if ((a <= 49 && b <= 57) || (a <= 50 && b <= 51))
 				{
-					for (d = 48; d <= 57; d++)
+					for (c = 48; c <= 53; c++)
 					{
-						_putchar(a);
-						_putchar(b);
-						_putchar(':');
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
+						for (d = 48; d <= 57; d++)
+						{
+							_putchar(a);
+							_putchar(b);
+							_putchar(':');
+							_putchar(c);
+							_putchar(d);
+							_putchar('\n');
+						}
 					}
 				}
-			}
+		}
 	}
-}
 }
