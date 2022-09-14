c48e6d276ae3e8b0adcc751584a21512bc328946
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 16:19:00 -0700

    Low Level Programming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack bauer - prints every minute of the day, starting from 00:00 -23:59
+ * jack_bauer - prints every minute of the day, starting from 00:00 to 23:59
  *
  * Return: prints time, 0 otherwise
  */
