5845f7b774754cfec5ced7ea632a4b62dbbf86ca
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 15:34:26 -0700

    Low Level Programming

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,9 @@
  * print_sign - prints the sign of a number
  * @n: number to be checked
  *
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero, -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ *		0 and prints 0 if n is zero
+ *		-1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
