22bb606f3c5744ec806256a2d45d65f514412920
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 18:51:41 -0700

    Advanced

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int a, b, c, d;
+
+	a = 1;
+	b = 2;
+	c = 3;
+
+	printf("%ld, ", a);
+	printf("%ld, ", b);
+
+	for (d =3; d < 50; d++)
+	{
+		printf("%ld, ", c);
+
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	printf("%ld\n", c);
+	return (0);
+}
+
