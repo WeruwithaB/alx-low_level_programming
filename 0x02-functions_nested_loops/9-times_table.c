d3cae6a8ae701d4814fbc7f37fc6e47c4e9eac91
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 18:00:54 -0700

    Low level Programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,46 @@
+#include <stdio.h>
 #include "main.h"
-#include <unistd.h>
 
 /**
- * times_table - prints the 9 times table, starting with 0
- * Return: prints multiples of 9, 0 otherwise
+ * times_table - function
+ *
+ * nested for loop
+ *
+ * if statement
+ *
+ * _putchar function
+ *
+ * Return: void
  */
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int n, m, times;
 
-	for (a = 0; a <= 9; a++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (m = 1; m <= 9; m++)
 		{
-			c = a * b;
-
-			if (c <= 9)
+			times = (m * n);
+			if ((times / 10) > 0)
 			{
-				_putchar(c + '0');
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
+				_putchar((times / 10) + '0')
 			}
-
 			else
 			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar(e + '0');
-				_putchar(d + '0');
-				_putchar(44);
-				_putchar(32);
+				_putchar(' ');
+			}
+			_putchar((times % 10) + '0');
+		
+			if (m < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
-
 		_putchar('\n');
 	}
 }
