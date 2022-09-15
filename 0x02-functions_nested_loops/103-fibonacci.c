1e955edc058dfb48abff3ee9f71f05c57609883c
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-14 18:59:51 -0700

    Advanced

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long count, a, b, c, sums;
+
+	a = sums = 0;
+	b = 1;
+
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sums += c;
+		}
+	}
+	printf("%lu\n", sums);
+	return (0);
+}
