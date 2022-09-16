9df79ddfed45375a80636aa7e054d048a3d6abbb
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-15 23:43:46 -0700

    C - Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - entry point of programs
+ *
+ * Return: return 0
+ */
+int main (void)
+{
+	int i;
+
+	if (i < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
