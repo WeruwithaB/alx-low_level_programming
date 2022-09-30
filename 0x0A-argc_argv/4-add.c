9e762f197043e730213f165d0e03935277cf8566
WeruwithaB <brandonwaweru24@gmail.com>
2022-09-30 15:42:05 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - adds positive numbers
- * @argc: number of arguments
- * @argv: array containing arguments passed to the program
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: 0 if no number is passed as argument
- * 	   1 and prints Error if one number contains a symbol
+ * Return: the int converted from the string
  */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
 
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main -adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int sum, num, i, j, k;
 
-	if (argc == 1)
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if ((argv[i] < '0' || *argv[i] > '9'))
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				printf("Error\n");
+				puts("Error");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
 		}
-		printf("%d\n", sum);
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
