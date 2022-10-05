0a97754b8d0cf34796baa0dd1bbdbac4c1ea2114
WeruwithaB <brandonwaweru24@gmail.com>
2022-10-04 18:28:22 -0700

    C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,9 +6,7 @@
  * with copy of a string given as a parameter
  * @str: string
  * Return: pointer to a new string which is a duplicate of the string str
- * NULL if str = NULL
- * pointer to duplicated string, NULL if insufficient memory was 
- * available
+ * NULL if str = NULL pointer to duplicated string, NULL if insufficient memory was available
  */
 
 char *_strdup(char *str)
